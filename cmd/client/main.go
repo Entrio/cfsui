3dabdca609be399b0e30809a8bcccf139f2aa773
Titarenko Alexander <westal@gmail.com>
2021-10-14 23:05:29 +0600

    Added test
    * Basic testing and benchmarking
    
    Signed-off-by: Titarenko Alexander <westal@gmail.com>

--- cmd/client/main.go
+++ cmd/client/main.go
@@ -1,51 +1,22 @@
 package main
 
 import (
-	"fmt"
 	"github.com/Entrio/cfsui/internal"
+	"os"
+
+	"github.com/rs/zerolog"
+	"github.com/rs/zerolog/log"
 )
 
 func main() {
 
-	s := internal.NewStack()
-	s.Push(1)
-	s.Push(2)
-	s.Push(3)
-	s.Push(4)
-	s.Push(5)
-	s.Push("A")
-	s.Push("B")
-	s.Push("C")
-	s.Push("D")
-	s.Push("E")
-	s.Push("F")
-	s.Push("G")
-	s.Push("H")
-	s.Push("I")
-	i := s.Pop().(string)
-	fmt.Println(i)
-	s.Pop()
-	s.Pop()
-	s.Pop()
-	s.Pop()
-	s.Pop()
-	s.Pop()
-	s.Pop()
-	s.Pop()
-	five := s.Pop().(int)
-	fmt.Println(five)
+	log.Logger = log.Output(zerolog.ConsoleWriter{Out: os.Stderr})
+	zerolog.TimeFieldFormat = zerolog.TimeFormatUnixMs
 
-	someValue := "this is cool"
-	s.Push(&someValue)
+	uim := internal.NewUIManager()
+	defer uim.Close()
 
-	peeked := s.Peek().(*string)
-	fmt.Println(fmt.Sprintf("Peek ptr: %d, value %s", peeked, *peeked))
-	*peeked = "pew pew"
+	go uim.RenderAndUpdate()
+	<-uim.WaitForExit()
 
-	strPtr := s.Pop().(*string)
-	fmt.Println(fmt.Sprintf("Pop ptr: %d, val: %s", strPtr, *strPtr))
-
-	//ui := internal.NewUIManager()
-	//go ui.Render()
-	//<-ui.WaitForExit()
 }
--- internal/ui_manager.go
+++ internal/ui_manager.go
@@ -1,21 +1,79 @@
 package internal
 
+import (
+	ui "github.com/gizak/termui/v3"
+	"github.com/gizak/termui/v3/widgets"
+	"github.com/rs/zerolog/log"
+)
+
 type (
 	UIManager struct {
 		quit    chan struct{}
 		Screens Stack
+		log     []string
 	}
 )
 
 func NewUIManager() *UIManager {
+	if err := ui.Init(); err != nil {
+		log.Fatal().Err(err).Msg("Failed to initialize termui")
+	}
 	return &UIManager{
 		quit:    make(chan struct{}),
 		Screens: NewStack(),
+		log:     []string{"Application started"},
 	}
 }
 
-// Render goroutine processes the input and rendering
-func (ui *UIManager) Render() {}
-func (ui *UIManager) WaitForExit() chan struct{} {
-	return ui.quit
+// RenderAndUpdate goroutine processes the input and rendering
+func (uim *UIManager) RenderAndUpdate() {
+	uim.clear()
+	uiEvents := ui.PollEvents()
+
+	for {
+		e := <-uiEvents
+		switch e.ID {
+		case "q", "<C-c>":
+			uim.quit <- struct{}{}
+			return
+		case "r":
+			uim.clear()
+		case "p", "<C-left>":
+		}
+	}
+
+}
+
+func (uim *UIManager) AddLog(data string) {}
+
+func (uim *UIManager) clear() {
+	ui.Clear()
+	tabpane := widgets.NewTabPane("Home", "Construction", "Production", "Market", "Population", "Military (10)")
+	tabpane.Title = "Beavertown"
+	tabpane.PaddingLeft = 2
+	tabpane.SetRect(0, 0, 75, 3)
+	tabpane.Border = true
+
+	p2 := widgets.NewParagraph()
+	p2.Text = "Current farms: 0\nCurrent food: 0"
+	p2.Title = "Keys"
+	p2.TextStyle.Fg = ui.ColorBlue
+	p2.SetRect(5, 5, 10, 10)
+	p2.BorderStyle.Fg = ui.ColorYellow
+
+	l := widgets.NewList()
+	l.Rows = uim.log
+	l.Title = "LOG"
+	l.SetRect(10, 7, 45, 15)
+
+	ui.Render(tabpane, l)
+}
+
+func (uim *UIManager) WaitForExit() chan struct{} {
+	return uim.quit
+}
+
+// Close performs all closing operations prior to program exit
+func (uim *UIManager) Close() {
+	ui.Close()
 }
